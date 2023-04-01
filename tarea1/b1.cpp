0f9514222c38671676cff4976de8a184d4fd7c3b
Nacho4756 <an.caldeguardia@gmail.com>
2023-04-01 00:32:05 -0300

    a

--- tarea1/b1.cpp
+++ tarea1/b1.cpp
@@ -4,24 +4,13 @@ using namespace std;
 int main(){
     long long n, m=0;
     cin>>n;
-    vector<long long> flies(n);
-    for(long long i = 0; i < n; i++){ // create vector with flies
+    queue<long long> flies;
+    for(long long i = 0; i < n; i++){
         long long a;
         cin>>a;
-        flies[i]=a;
+        flies.push(a);
     }
-    for(long long i = 0; i < n; i++){
-        set<long long> max_flies;
-        long long s;
-        for(long long j = 0; j < n; j++){
-            s = max_flies.size();
-            max_flies.insert(flies[j]);
-            if(s==max_flies.size()){
-                if(m<s) m==s;
-                break; //alo
-            }
-        }
-    }
+    
     cout<<m<<endl;
     return 0;
 }

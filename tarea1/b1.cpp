0b475af4fe8c55ccfe4366035e4dab76ee8daa61
Nacho4756 <an.caldeguardia@gmail.com>
2023-04-01 02:21:36 -0300

    b1 completed

--- tarea1/b1.cpp
+++ tarea1/b1.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 int main(){
-    long long n, m=0;
+    ll n, m=0, i=0;
     cin>>n;
-    queue<long long> flies;
-    for(long long i = 0; i < n; i++){
-        long long a;
+    map<ll, ll> occurrence;
+    for (ll j = 0; j < n; j++) {
+        ll a;
         cin>>a;
-        flies.push(a);
+        i = max(i, occurrence[a-1]);
+        m = max(m, j - i + 1);
+        occurrence[a-1] = j+1;
     }
-    
     cout<<m<<endl;
     return 0;
 }

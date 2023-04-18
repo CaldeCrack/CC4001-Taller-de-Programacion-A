cfc1c6d65921734cf231602fc45ce31c8be193e8
Nacho4756 <an.caldeguardia@gmail.com>
2023-04-18 00:50:10 -0400

    c2 finished

--- tarea2/c2.cpp
+++ tarea2/c2.cpp
@@ -1,22 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int s, vector<vector<int>> &grafo, vector<int> &visited){
+void dfs(int s, map<int, vector<int>> &grafo, map<int, int> &visited){
     visited[s]=1;
     for(int v:grafo[s]) if(!visited[v]) dfs(v, grafo, visited);
 }
 
 int main(){
-    int n, d=0;
+    int n, x2, d=0;
     cin>>n;
-    map<int, vector<int>> grafo(2*n);
-    map<int> visited(2*n, 0);
+    map<int, vector<int>> grafo;
+    map<int, int> visited;
     while(n--){
         int x, y;
         cin>>x>>y;
-
+        grafo[x].push_back(y+1000);
+        grafo[y+1000].push_back(x);
+        x2=x;
     }
-    for(int i=0; i<n; i++){
-
+    dfs(x2, grafo, visited);
+    for (auto i = grafo.begin(); i != grafo.end(); i++) {
+        if(!visited[i->first]){
+            dfs(i->first, grafo, visited);
+            d++;
+        }
     }
+    cout<<d<<endl;
+    return 0;
 }

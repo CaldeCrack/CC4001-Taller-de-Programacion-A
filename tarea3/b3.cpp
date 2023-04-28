93b1949621c6d70edbfbd0d35ebe0ad8b5a1320e
Nacho4756 <an.caldeguardia@gmail.com>
2023-04-28 13:08:05 -0400

    started b3

---
+++ tarea3/b3.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll INF = 1e18;
+
+int main(){
+	int n, m; cin >> n >> m;
+	using state = pair<ll,int>;
+	vector <vector <state>> adj(n); 
+	for(int i=0; i<m; i++){
+		int u, v;
+		ll w;
+		cin >> u >> v >> w;
+		u--;
+		v--;
+		adj[u].push_back({v, w});
+		adj[v].push_back({u, w});
+	}
+	priority_queue <state, vector<state>, greater<state>> pq;
+	vector <ll> dist(n, INF);
+    map <ll, ll> path;
+	pq.push({0,0});
+	dist[0] = 0;
+	while(!pq.empty()){
+		auto [peso_camino, u] = pq.top();
+		pq.pop();
+		if(peso_camino != dist[u]) continue;
+		for(auto [v, w] : adj[u]){
+			if(peso_camino + w < dist[v]){
+				dist[v] = peso_camino+w;
+				pq.push({dist[v], v});
+                path[u] = v;
+			}
+		}
+	}
+    if(dist[n-1] == INF){
+        cout<<-1<<endl;
+        return 0;
+    }
+    for(auto i = path.begin(); i != path.end(); i++) cout<<(i -> second)<<" ";
+	return 0;
+}

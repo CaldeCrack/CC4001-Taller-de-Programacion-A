76e9a3afde91f58757ab591509afd283bcc17ec2
Nacho4756 <an.caldeguardia@gmail.com>
2023-05-02 21:38:54 -0400

    tarea3 finished

--- tarea3/a3.cpp
+++ tarea3/a3.cpp
@@ -5,41 +5,33 @@ typedef long long ll;
 using state = pair<ll,ll>;
 ll INF = 1e18;
 
-void dijkstra(int s, int n, vector<state> adj[], vector<ll> &dist){
-	dist.resize(n, INF);
+void dijkstra(int s, vector<vector<state>> &adj, vector<ll> &dist){
 	dist[s] = 0;
-	priority_queue<state, vector<state>,greater<state>> pq;
-	pq.push({dist[s], s});
+	priority_queue<state, vector<state>, greater<state>> pq;
+	pq.push({0, s});
 	while(!pq.empty()){
-		int u = pq.top().second;
+		auto [peso, u] = pq.top();
 		pq.pop();
+		if(peso != dist[u]) continue;
 		for(auto [v, w]:adj[u]){
-			if(dist[u]+w < dist[v]){
-				dist[v] = dist[u]+w;
+			if(peso+w < dist[v]){
+				dist[v] = peso+w;
 				pq.push({dist[v], v});
 			}
 		}
 	}
 }
 
-void minCostPath(vector<pair<ll, state>> &edges, int n, int m){
-	vector<state> adj[n+1];
-	for(int i=0; i<m; i++){
-		ll x = edges[i].first;
-        auto [y, z] = edges[i].second;
-		adj[x].push_back({y,z});
-		adj[y].push_back({x,z});
-	}
-	vector<ll> dist_from_s;
-	vector<ll> dist_from_d;
-	dijkstra(1, n+1, adj, dist_from_s);
-	dijkstra(n, n+1, adj, dist_from_d);
+void minCostPath(vector<vector<state>> &adjI, vector<vector<state>> &adjD, int n, int m){
+	vector<ll> dist_from_s(n+1, INF);
+	vector<ll> dist_from_d(n+1, INF);
+	dijkstra(1, adjI, dist_from_s);
+	dijkstra(n, adjD, dist_from_d);
 	ll min_cost = dist_from_s[n];
-	for(auto &it:edges){
-		ll u = it.first;
-        auto [v,c] = it.second;
-		ll cur_cost = dist_from_s[u]+c/2+dist_from_d[v]; // agradecido maximo
-		min_cost = min(min_cost, cur_cost);
+	for(int i=0; i<adjI.size(); i++){
+        for(auto [v,c]:adjI[i]){
+			min_cost = min(min_cost, dist_from_s[i]+c/2+dist_from_d[v]);
+		}
 	}
 	cout<<min_cost<<endl;
 }
@@ -47,12 +39,14 @@ void minCostPath(vector<pair<ll, state>> &edges, int n, int m){
 int main(){
 	int n, m;
 	cin>>n>>m;
-	vector<pair<ll, state>> edges;
+	vector<vector<state>> adjI(n+1);
+	vector<vector<state>> adjD(n+1);
 	for(int i=0; i<m; i++){
 		ll a, b, c;
 		cin>>a>>b>>c;
-		edges.push_back({a, {b,c}});
+		adjI[a].push_back({b,c});
+		adjD[b].push_back({a,c});
 	}
-	minCostPath(edges, n, m);
+	minCostPath(adjI, adjD, n, m);
 	return 0;
 }

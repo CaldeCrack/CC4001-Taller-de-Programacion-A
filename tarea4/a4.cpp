1a3716742a67d8aada3b630ed9ca80c1ec0fe429
Nacho4756 <an.caldeguardia@gmail.com>
2023-05-27 23:01:40 -0400

    a done

---
+++ tarea4/a4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n, m; cin>>n; m=n; n<<=1;
+        double sum=0;
+        priority_queue<double> diamonds;
+        priority_queue<double> miners;
+        while(n--){
+            int x, y;
+            cin>>x>>y;
+            if(x) diamonds.push(abs(x));
+            else miners.push(abs(y));
+        }
+        while(m--){
+            sum += sqrt(pow(diamonds.top(), 2)+pow(miners.top(), 2));
+            diamonds.pop(); miners.pop();
+        }
+        cout<<fixed<<setprecision(10)<<sum<<endl;
+    }
+}

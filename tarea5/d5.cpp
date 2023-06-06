cc72842144a334ff14e706d4f4752cf53b318191
Nacho4756 <an.caldeguardia@gmail.com>
2023-06-05 22:36:33 -0400

    d5 finished

---
+++ tarea5/d5.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ll n, sum=0; cin>>n;
+    for (ll i=1; i<=sqrt(n); i++) sum+=(n/i-i+1)*i+(((n/i)*(n/i+1))>>1)-((i*(i+1))>>1);
+    cout<<sum%1000000007<<endl;
+    return 0;
+}

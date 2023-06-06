002db393e10afbc145fc38eaff34c46725084074
Nacho4756 <an.caldeguardia@gmail.com>
2023-06-05 23:11:57 -0400

    aprendan a escribir

--- tarea5/d5.cpp
+++ tarea5/d5.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 typedef long long ll;
 
+int linearSum(int n){
+    return (n * (n + 1) / 2) % 1000000007;
+}
+
+int rangeSum(int b, int a){
+    return (linearSum(b) - linearSum(a)) % 1000000007;
+}
+
 int main(){
-    ll n, sum=0; cin>>n;
-    for(ll i=1; i<=sqrt(n); i++) sum+=(n/i-i+1)*i+(((n/i)*(n/i+1))/2)-((i*(i+1))/2);
-    cout<<sum%1000000007<<endl;
+    ll n, sum=0, i=1; cin>>n;
+    while(1){
+        sum += rangeSum(n / i, n / (i + 1)) * (i % 1000000007) % 1000000007;
+        sum %= 1000000007;
+        if(i==n) break;
+        i = n/(n/(i+1));
+    }
+    cout<<sum<<endl;
     return 0;
 }

974da4cc40db8ed7c8a84fc8f50446b3f749db17
Nacho4756 <an.caldeguardia@gmail.com>
2023-06-05 22:48:58 -0400

    xd

--- tarea5/d5.cpp
+++ tarea5/d5.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 
 int main(){
     ll n, sum=0; cin>>n;
-    for (ll i=1; i<=sqrt(n); i++) sum+=(n/i-i+1)*i+(((n/i)*(n/i+1))>>1)-((i*(i+1))>>1);
+    for(ll i=1; i<=sqrt(n); i++) sum+=(n/i-i+1)*i+(((n/i)*(n/i+1))/2)-((i*(i+1))/2);
     cout<<sum%1000000007<<endl;
     return 0;
 }

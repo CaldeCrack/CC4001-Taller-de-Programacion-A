bed84273c9667ceb66724d0242484400d7d1b40d
Nacho4756 <an.caldeguardia@gmail.com>
2023-06-05 23:17:50 -0400

    puede ser pa

--- tarea5/d5.cpp
+++ tarea5/d5.cpp
@@ -3,22 +3,18 @@ using namespace std;
 
 typedef long long ll;
 
-int linearSum(int n){
-    return (n * (n + 1) / 2) % 1000000007;
-}
-
-int rangeSum(int b, int a){
-    return (linearSum(b) - linearSum(a)) % 1000000007;
+int sum(int n){
+    return (n*(n+1)/2)%1000000007;
 }
 
 int main(){
-    ll n, sum=0, i=1; cin>>n;
+    ll n, res=0, i=1; cin>>n;
     while(1){
-        sum += rangeSum(n / i, n / (i + 1)) * (i % 1000000007) % 1000000007;
-        sum %= 1000000007;
+        res += ((sum(n/i)-sum(n/(i+1)))%1000000007)*(i%1000000007)%1000000007;
+        res %= 1000000007;
         if(i==n) break;
         i = n/(n/(i+1));
     }
-    cout<<sum<<endl;
+    cout<<res<<endl;
     return 0;
 }

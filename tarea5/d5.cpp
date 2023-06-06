3d0e4a8647dfb624767bb1d993ca2b5ac96790cf
Nacho4756 <an.caldeguardia@gmail.com>
2023-06-05 23:26:49 -0400

    ya xd nomas

--- tarea5/d5.cpp
+++ tarea5/d5.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 typedef long long ll;
+int mod=1000000007;
 
-int sum(int n){
-    return (n*(n+1)/2)%1000000007;
+int sum(ll m){
+    return (m*(m+1)/2)%mod;
 }
 
 int main(){
     ll n, res=0, i=1; cin>>n;
     while(1){
-        res += ((sum(n/i)-sum(n/(i+1)))%1000000007)*(i%1000000007)%1000000007;
-        res %= 1000000007;
+        res += ((sum(n/i)-sum(n/(i+1)))%mod)*(i%mod)%mod;
+        res %= mod;
         if(i==n) break;
         i = n/(n/(i+1));
     }
